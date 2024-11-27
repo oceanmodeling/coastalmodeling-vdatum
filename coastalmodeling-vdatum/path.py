b91077f8a1a3d241b88ca39ecaf401e9a01c928b
Felicio Cassalho <61760479+felicio93@users.noreply.github.com>
2024-11-27 13:42:33 -0500

    Create path.py

---
+++ coastalmodeling-vdatum/path.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+xGEOID20B=f"C:/Users/Felicio.Cassalho/Downloads/xGEOID20B.tif"
+MLLW_ITRF2020_2020=f"https://noaa-ocs-nationalbathymetry-pds.s3.amazonaws.com/Test-and-Evaluation/NOAA_NOS_Vertical_Datum_Register/national_model_grids/us_noaa_nos_MLLW-ITRF2020_2020.0_%28nwldatum_4.7.0_20240621%29.tif"
+LMSL_ITRF2020_2020=f"https://noaa-ocs-nationalbathymetry-pds.s3.amazonaws.com/Test-and-Evaluation/NOAA_NOS_Vertical_Datum_Register/national_model_grids/us_noaa_nos_LMSL-ITRF2020_2020.0_%28nwldatum_4.7.0_20240621%29.tif"
