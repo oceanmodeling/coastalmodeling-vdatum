8aefa009ec0d3235d968e49105e62a09441ace98
Felicio Cassalho <61760479+felicio93@users.noreply.github.com>
2025-06-12 12:50:23 -0400

    Update _path.py

--- coastalmodeling_vdatum/_path.py
+++ coastalmodeling_vdatum/_path.py
@@ -6,4 +6,4 @@ MLLW_ITRF2020_2020=f"https://noaa-nos-stofs2d-pds.s3.amazonaws.com/_archive/coas
 MLW_ITRF2020_2020=f"https://noaa-nos-stofs2d-pds.s3.amazonaws.com/_archive/coastalmodeling-vdatum/us_noaa_nos_MLW-ITRF2020_2020.0_nwldatum_4.7.0_20240621_.tif"
 MHW_ITRF2020_2020=f"https://noaa-nos-stofs2d-pds.s3.amazonaws.com/_archive/coastalmodeling-vdatum/us_noaa_nos_MHW-ITRF2020_2020.0_nwldatum_4.7.0_20240621_.tif"
 LMSL_ITRF2020_2020=f"https://noaa-nos-stofs2d-pds.s3.amazonaws.com/_archive/coastalmodeling-vdatum/us_noaa_nos_LMSL-ITRF2020_2020.0_nwldatum_4.7.0_20240621_2.tif"
-SGEOID2022=f"https://noaa-nos-stofs2d-pds.s3.amazonaws.com/_archive/coastalmodeling-vdatum/us_noaa_sgeoid2022v1a.tif"
+# SGEOID2022=f"https://noaa-nos-stofs2d-pds.s3.amazonaws.com/_archive/coastalmodeling-vdatum/us_noaa_sgeoid2022v1a.tif"
