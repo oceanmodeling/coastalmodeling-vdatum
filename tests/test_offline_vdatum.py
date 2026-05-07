ee404898ef3e73c3ff55e4fce08c83c661983137
felicio93 <felicio.cassalho@noaa.gov>
2026-05-07 19:15:48 +0000

    refactor: Remove large files and download them in tests

--- tests/test_offline_vdatum.py
+++ tests/test_offline_vdatum.py
@@ -4,17 +4,25 @@ import pytest
 from coastalmodeling_vdatum import vdatum, _path
 import numpy
 
+import pytest
+from coastalmodeling_vdatum import vdatum, _path
+import numpy
+import os
+import tempfile
+import subprocess
+
 @pytest.fixture(scope="module")
 def setup_paths():
-    # This fixture can be used to download the necessary files if they don't exist
-    # For now, it assumes the files are present at a known location.
-    # In a real-world scenario, you might want to automatically download these from
-    # https://noaa-nos-stofs2d-pds.s3.amazonaws.com/index.html#_archive/coastalmodeling-vdatum/
-    PATH = "/home/Felicio.Cassalho/coastalmodeling-vdatum/geotiff_files" # IMPORTANT: Update this path
-    _path.NAVD88_G2018 = f"{PATH}/us_noaa_g2018u0.tif"
-    _path.XGEOID20B = f"{PATH}/xGEOID20B.tif"
-    _path.MLLW_ITRF2020_2020 = f"{PATH}/us_noaa_nos_MLLW-ITRF2020_2020.0_nwldatum_4.7.0_20240621_2.tif"
-    _path.LMSL_ITRF2020_2020 = f"{PATH}/us_noaa_nos_LMSL-ITRF2020_2020.0_nwldatum_4.7.0_20240621_2.tif"
+    with tempfile.TemporaryDirectory() as tmpdir:
+        subprocess.run(["wget", "-P", tmpdir, "https://noaa-nos-stofs2d-pds.s3.amazonaws.com/_archive/coastalmodeling-vdatum/us_noaa_g2018u0.tif"])
+        subprocess.run(["wget", "-P", tmpdir, "https://noaa-nos-stofs2d-pds.s3.amazonaws.com/_archive/coastalmodeling-vdatum/xGEOID20B.tif"])
+        subprocess.run(["wget", "-P", tmpdir, "https://noaa-nos-stofs2d-pds.s3.amazonaws.com/_archive/coastalmodeling-vdatum/us_noaa_nos_MLLW-ITRF2020_2020.0_nwldatum_4.7.0_20240621_2.tif"])
+        subprocess.run(["wget", "-P", tmpdir, "https://noaa-nos-stofs2d-pds.s3.amazonaws.com/_archive/coastalmodeling-vdatum/us_noaa_nos_LMSL-ITRF2020_2020.0_nwldatum_4.7.0_20240621_2.tif"])
+        _path.NAVD88_G2018 = os.path.join(tmpdir, "us_noaa_g2018u0.tif")
+        _path.XGEOID20B = os.path.join(tmpdir, "xGEOID20B.tif")
+        _path.MLLW_ITRF2020_2020 = os.path.join(tmpdir, "us_noaa_nos_MLLW-ITRF2020_2020.0_nwldatum_4.7.0_20240621_2.tif")
+        _path.LMSL_ITRF2020_2020 = os.path.join(tmpdir, "us_noaa_nos_LMSL-ITRF2020_2020.0_nwldatum_4.7.0_20240621_2.tif")
+        yield
 
 # @pytest.mark.parametrize("vfrom, vto, lat, lon, result", [
 #     ('lmsl', 'mlw', 30.197168, -87.842459, 0.183),
